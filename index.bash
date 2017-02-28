ffd6f65150b8efadde5098c72f54c0be9b0c1716
Tyson Acker <tyson@devmode.com>
2017-02-28 12:50:46 -0600

    Better .env existence checking

--- index.bash
+++ index.bash
@@ -2,16 +2,23 @@
 
 if [ -n "$1" ]
 then # path to .env provided
-  BASEDIR=${1%/} # remove trailing slash
-else # no .env path specified; use PWD
-  BASEDIR=.
+  BASEDIR=$1
+else # no path specified; use PWD
+  BASEDIR=`pwd`
 fi
+BASEDIR=${BASEDIR%/} # remove trailing slash
 
 # UPPERCASE package name, replace nonalphanumeric chars with '_'
 PACKAGE=`echo $npm_package_name | tr [:lower:] [:upper:] | sed 's/[^a-zA-Z0-9]/_/g'`
 
-if [ ! -f $BASEDIR/.env ] # make sure .env file exists
+if [ -e $BASEDIR/.env ] # make sure .env file exists
 then
+  if [ ! -f $BASEDIR/.env ]
+  then
+    echo "npm: docker-env-version: $BASEDIR/.env exists but is not a regular file?"
+    exit 1
+  fi
+else
   touch $BASEDIR/.env
 fi
 
